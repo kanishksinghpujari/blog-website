76872de8a9f63436609a7773130577026674e3d0
kanishksinghpujari <68298682+kanishksinghpujari@users.noreply.github.com>
2021-06-03 01:44:27 +0530

    second

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ app.use(express.static("public"));
 app.get("/",function(req,res){
   res.render("home",{content:homeStartingContent});
 });
+app.get("/about",function(req,res){
+  res.render("about",{content:aboutContent});
+});
+app.get("/contact",function(req,res){
+  res.render("contact",{content:contactContent});
+});
+
 
 
 
