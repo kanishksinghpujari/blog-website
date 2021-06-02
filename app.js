08f8932c255e2741609c9816257574f12205962e
kanishksinghpujari <68298682+kanishksinghpujari@users.noreply.github.com>
2021-06-03 02:18:23 +0530

    third

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
+var posts=[];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,13 +18,25 @@ app.use(express.static("public"));
 
 
 app.get("/",function(req,res){
-  res.render("home",{content:homeStartingContent});
+  res.render("home",{content:homeStartingContent,posts:posts});
 });
 app.get("/about",function(req,res){
   res.render("about",{content:aboutContent});
 });
 app.get("/contact",function(req,res){
   res.render("contact",{content:contactContent});
+});
+app.get("/compose",function(req,res){
+  res.render("compose" );
+});
+
+app.post("/compose",function(req,res){
+   let new1={
+    title:req.body.newT,
+   content:req.body.newC
+   };
+   posts.push(new1);
+   res.redirect("/");
 });
 
 
